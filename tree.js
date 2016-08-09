d88966a809a57e72ae2c4f861369fe9e744be34e
eest9 <eestoneee@gmail.com>
2016-08-10 01:24:55 +0200

    more tree logic

--- tree.js
+++ tree.js
@@ -1,14 +1,19 @@
-function test() {
-	var object = {
-		name : '',
-		available : 1
-	};
-	object.available = 1;//überprüfe die CSV auf weiter Objekte
-	object.name = 'oh';//hole den Namen aus der csv
-	do{
+function tree() {
+	function en81346_object(aspect_function, aspect_location, aspect_product) {
+		this.aspect_function = aspect_function;
+		this.aspect_location = aspect_location;
+		this.aspect_product = aspect_product;
+	}
+	
+	var item = [
+	    new en81346_object("=PSU","+C01","-UC1"),
+	    new en81346_object("=PSU","+C01","-UC2"),
+	    new en81346_object("=PSU","+C01","-UC3")
+	];
+	var amount_of_items = item.length;
+	for(i = 0; i < amount_of_items; i++){
 		document.write('<li>');
-		document.write(object.name);
+		document.write(item[i].aspect_product);
 		document.write('</li>');
-		object.available = 0;
-	}while(object.available == 1);
+	};
 }
